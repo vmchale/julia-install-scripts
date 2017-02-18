02b7120bc0603d1bd0b8fd56d2e7f7a3c2820b20
Vanessa McHale <tmchale@wisc.edu>
2017-02-18 16:20:00 -0600

    other stuff + sh

---
+++ README.md
@@ -0,0 +1,3 @@
+## Usage
+
+Install script for 0.5.0 binary release of the julia language
---
+++ julia-installer.sh
@@ -0,0 +1,4 @@
+curl -sS https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.0-linux-x86_64.tar.gz -o julia-0.5.0.tar.gz
+tar -xvf julia-0.5.0.tar.gz
+# delete/ask to reinstall?
+mv julia-3c9d75391c/ ~/.julia/
